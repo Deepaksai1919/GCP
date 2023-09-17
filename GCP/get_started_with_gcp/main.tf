b16183aecb992059f408b04a636797ddcb82fb46
Deepak <saideepak220@gmail.com>
2023-09-17 15:36:59 +0530

    Getting Started

---
+++ GCP/get_started_with_gcp/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.82.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "terraform-399209"
+  region = "us-central1"
+  zone = "us-centra1-a"
+}
+
+resource "google_storage_bucket" "GCS1" {
+  name = "bucket-from-tf-up-deepak"
+  location = "US"
+}
