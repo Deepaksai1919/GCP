91687d81a4ca0befb389131870c463c70f0ced31
Deepak <saideepak220@gmail.com>
2023-09-17 20:22:09 +0530

    ClouRun:Terraform

---
+++ GCP/3.CloudRun/main.tf
@@ -0,0 +1,25 @@
+resource "google_cloud_run_v2_service" "run-app-from-tf" {
+  name = "run-app-from-tf"
+  location = "asia-southeast1"
+  template {
+    containers {
+      image = "gcr.io/google-samples/hello-app:1.0"
+    }
+    service_account = "svc-terraform@terraform-399209.iam.gserviceaccount.com"
+  }
+}
+
+# For No Auth / Public Access
+data "google_iam_policy" "no_auth" {
+  binding {
+    role = "roles/run.invoker"
+    members = ["allUsers"]
+  }
+}
+
+resource "google_cloud_run_v2_service_iam_policy" "no_auth" {
+  name = google_cloud_run_v2_service.run-app-from-tf.name
+  location = google_cloud_run_v2_service.run-app-from-tf.location
+  policy_data = data.google_iam_policy.no_auth.policy_data
+}
+
