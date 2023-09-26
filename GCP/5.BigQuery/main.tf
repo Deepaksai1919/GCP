5f1f21c4db2fe8e4c7c3e658580fea66d4789f2c
Deepaksai1919 <31119831+Deepaksai1919@users.noreply.github.com>
2023-09-26 09:12:13 +0530

    BigQuery:Terraform

---
+++ GCP/5.BigQuery/main.tf
@@ -0,0 +1,8 @@
+resource "google_bigquery_dataset" "bq_ds" {
+  dataset_id = "ds_from_tf"
+}
+
+resource "google_bigquery_table" "bq_table" {
+  dataset_id = google_bigquery_dataset.bq_ds.dataset_id
+  table_id = "table_from_tf"
+}
---
+++ GCP/5.BigQuery/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.82.0"
+    }
+  }
+}
+
+provider "google" {
+  # Configuration options
+  project = "terraform-399209"
+  region = "us-central1"
+  zone = "us-centra1-a"
+  credentials = "../../terraform-svc.json"
+}
