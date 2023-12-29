e3dfd543fa3765d62b9b1b735156917ea0475e72
Deepaksai1919 <31119831+Deepaksai1919@users.noreply.github.com>
2023-12-29 08:55:52 +0530

    build config change

--- sample/tests/test_func.py
+++ sample/tests/test_func.py
@@ -1,4 +1,4 @@
-from app.app import some_function
+from sample.app import some_function
 def test_some_function():
-    nums = [i for i in range(11)]
+    nums = [i for i in range(10)]
     assert some_function(*nums) == 55
