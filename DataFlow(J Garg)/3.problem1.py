eb27da7e9396bcc55ebe18512903030503c75b78
Deepaksai1919 <31119831+Deepaksai1919@users.noreply.github.com>
2023-12-30 20:12:02 +0530

    Corrected the code

--- DataFlow(J Garg)/3.problem1.py
+++ DataFlow(J Garg)/3.problem1.py
@@ -8,6 +8,7 @@ PCollection = (
     pipeline
     | 'Read the Input' >> beam.io.ReadFromText('datasets/dept_data.txt')
     | 'Split the string to list' >> beam.Map(lambda record: record.strip().split(','))
+    | 'Filter the employees in Accounts section' >> beam.Filter(lambda record: record[3] == 'Accounts' )
     | 'Select the employee name and attendance' >> beam.Map(lambda record: (record[1], int(record[2])))
     | 'Combine Per Key' >> beam.CombinePerKey(sum)
     | 'Write result tot output' >> beam.io.WriteToText('outputs/problem1/out')
